b44407775eb2a489929ad471ecb577c1dfbf32f9
Chapa.C(Takuya) <chapatown@gmail.com>
2018-12-01 20:17:20 +0900

    VO, DTOの基底作成

---
+++ Assets/Scripts/Common/DTOBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public abstract class DTOBase
+    {
+        protected VOBase _vo;
+
+        public void SetVO(VOBase vo)
+        {
+            _vo = vo;
+        }
+    }
+}
---
+++ Assets/Scripts/Common/VOBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public abstract class VOBase
+    {
+    }
+}
