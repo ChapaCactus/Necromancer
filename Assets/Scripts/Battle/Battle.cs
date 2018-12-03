b4e6aee683cecbef87853fa3c509f8d200a5c16c
Chapa.C(Takuya) <chapatown@gmail.com>
2018-12-03 22:03:15 +0900

    GameManager, CharacterManager追加、バトルシーン配置、クラス追加

---
+++ Assets/Scripts/Battle/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public class Battle : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if(!GameManager.IsInitialized)
+            {
+                GameManager.Initialize();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG
+{
+    public static class GameManager
+    {
+        public static CharacterManager CharacterManager { get; private set; }
+
+        public static bool IsInitialized { get; private set; } = false;
+
+        public static void Initialize()
+        {
+            IsInitialized = false;
+
+            CharacterManager = new CharacterManager();
+
+            IsInitialized = true;
+
+            Debug.Log("Initialized.");
+        }
+    }
+}
